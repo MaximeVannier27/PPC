6ea4b89025b682738f948f89c63c37d7a34b688a
eoliennne <likavf3@gmail.com>
2024-01-23 14:04:30 +0100

    premier commit

--- fonction_serveur.py
+++ fonction_serveur.py
@@ -2,3 +2,19 @@ def main_server():
     print("main_server")
 
     # Code pour demander le nombre de joueurs, créer les processus, etc.
+
+def piocheCarte(pioche):
+    return
+
+pioche = []
+
+def distribution(nombre_joueurs):
+    couleurs = ["rouge","vert","bleu","jaune","blanc"]
+    global pioche
+    for color in range(nombre_joueurs):
+        pioche+= [(1,couleurs[color])]*3
+        pioche+= [(2,couleurs[color]),(3,couleurs[color]),(4,couleurs[color])]*2
+        pioche+= [(5,couleurs[color])]
+    print(pioche)
+
+    
--- main_serveur.py
+++ main_serveur.py
@@ -1,35 +1,57 @@
 import socket
-from threading import Lock
-from multiprocessing import Process, Manager, Queue, Value
+from threading import Lock,Thread
+from multiprocessing import Process, Manager, Value
+from multiprocessing Lock as mLock
 from joueur_process import joueur_process
-from fonction_serveur import main_server
-
+from fonction_serveur import main_server, distribution,piocheCarte
+import threading      
+from sysv_ipc import MessageQueue,IPC_CREAT
+     
+    
+    
 
 if __name__ == "__main__":
 
     nombre_joueurs = int(input("Combien de joueurs vont se connecter ?\n--> "))
-    dic_joueurs = {}
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('localhost', 8000))
     server_socket.listen(nombre_joueurs)
 
-    shared_memory = Manager().dict()
-    message_queue = Queue()
+    #initialisation des variables
+    mains = Manager().dict()
+    tour = Value('I',(1,),lock=False)
+    n_indices = nombre_joueurs + 3
+    indices = Value('I',(n_indices,),lock=False)
+    shared_memory = {"indices":indices,"mains":mains,"tour":tour}
+    
+    erreurs = 3 
+    dic_mq = {}
+    for i in range(1,nombre_joueurs+1):
+        dic_mq[f"{i}"]=MessageQueue(i, sysv_ipc.IPC_CREAT)
 
-
+    thread_pioche = Thread(target=distribution,args=(nombre_joueurs,))
+    thread_pioche.start()
     c=1
-
+    dic_joueurs = {}
     while c<=nombre_joueurs:
         print("En l'attente de joueurs...")
         client, addr = server_socket.accept()
         print(f"Joueur {c} connecté ({addr})")
-        joueur = Process(target=joueur_process, args=(shared_memory, message_queue,client))
+        joueur = Process(target=joueur_process, args=(c,shared_memory, dic_mq,client,synchro))
         joueur.start()
         dic_joueurs[f"joueur_{c}"] = {"client":client,"addresse":addr,"process":joueur}
+        
         c+=1
+    thread_pioche.join()
+
+    
+
+    #envoi d'un bit pour signaler le début de la partie
     for _,lst in dic_joueurs.items():
           lst["client"].sendall(b'\x01')
+    
+
 
     main_server()
     server_socket.close()
